fe572cc4299261d109235a59e9789d033c4b552e
Constantin <mordredur@googlemail.com>
2019-12-04 13:48:09 +0100

    refactoring

--- src/days/day4.clj
+++ src/days/day4.clj
@@ -19,3 +19,10 @@
     (as-> str-n s
       (map #(<= (Integer/parseInt (str %1)) (Integer/parseInt (str  %2))) s (drop 1 s))
       (every? true? s))))
+
+(def fulfil-rules (every-pred six-digits? two-same-adj? increasing?))
+
+(defn solve []
+  (as-> (range start end) nums
+    (filter fulfil-rules nums)
+    (count nums)))
