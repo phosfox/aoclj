eadca4f7f4dc7cb5ccb7963c52787d69ca342489
Constantin <mordredur@googlemail.com>
2019-12-04 13:04:14 +0100

    adds predicates

---
+++ src/days/day4.clj
@@ -0,0 +1,21 @@
+(ns days.day4)
+
+(def start 172851)
+
+(def end 675869)
+
+(defn six-digits? [n]
+  (if (= 6 (count (str n))) true false))
+
+(defn two-same-adj? [n]
+  (let [str-n (str n)]
+    (as-> str-n s
+      (map #(= %1 %2) (drop 1 s) s)
+      (some true? s)
+      (if s true false))))
+
+(defn increasing? [n]
+  (let [str-n (str n)]
+    (as-> str-n s
+      (map #(<= (Integer/parseInt (str %1)) (Integer/parseInt (str  %2))) s (drop 1 s))
+      (every? true? s))))
